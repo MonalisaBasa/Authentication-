624bb58126774fdd3b7c58030f519c75823d9bcc
MonalisaBasa <monalisa.basa.mb@gmail.com>
2023-10-24 23:57:48 +0530

    Implemented Login

--- components/Auth/AuthForm.js
+++ components/Auth/AuthForm.js
@@ -21,42 +21,50 @@ const AuthForm = () => {
 
     //optional :Add validation
 
+    setIsLoading(true);
+    let url;
     if (isLogin) {
-      setIsLoading(true);
+      url='https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAy1x8U_13KlYqai3cwii7J-Hdy4Wv4PMM'
 
     } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAy1x8U_13KlYqai3cwii7J-Hdy4Wv4PMM',
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredpassword,
-            returnSecureToken: true
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-      ).then(res => {
-        if (res.ok) {
-          setIsLoading(false);
-
-        } else {
-          return res.json().then((data) => {
-            //show an error modal
-            // console.log(data);
-            let errorMessage = 'Authentication failed';
-            if(data && data.error && data.error.message){
-              errorMessage = data.error.message;
-            };
-            alert(errorMessage);
-          });
-        
-        };
-      });
-
-
+     url='https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAy1x8U_13KlYqai3cwii7J-Hdy4Wv4PMM'
     };
+    fetch(url,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredpassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  ).then((res) => {
+    setIsLoading(false);
+    if (res.ok) {
+      return res.json();
+
+    } else {
+      return res.json().then((data) => {
+        //show an error modal
+        // console.log(data);
+        let errorMessage = 'Authentication failed';
+        // if(data && data.error && data.error.message){
+        //   errorMessage = data.error.message;
+        // };
+        // alert(errorMessage);
+        throw new Error(errorMessage);
+      });
+    };
+  })
+  .then((data) =>{
+    console.log(data);
+  })
+  .catch((err) => {
+    alert(err.message);
+    });
   }
 
     return (
